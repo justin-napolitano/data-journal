b0bb3f84b1935ae89e9b2ce6af8cad3a514ec1ab
Justin <jnapolitano@Justins-MacBook-Pro.local>
2021-10-16 16:27:50 -0500

    changed-theme-to-book

--- source/conf.py
+++ source/conf.py
@@ -47,9 +47,18 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_book_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Theme Configuration
+html_theme_options = {
+"repository_url": "github.com/jusitn-napolitano/data-journal",
+"use_respository_button": True,
+"use_download_button": True,
+"home_page_in_toc": True,
+"use_fullscreen_button": True
+}
